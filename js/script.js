ecc3c33ea5af6795c3021c5ea502879d46e99bad
Arafin Ahmed <arafin2021@gmail.com>
2021-01-26 12:06:22 +0600

    function

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 //Login button event handler
 const loginBtn = document.getElementById('verify-login');
 loginBtn.addEventListener('click', function(){
-    console.log('Clicked');
     const logIn = document.getElementById('login-area');
     logIn.style.display = 'none';
     const transactionArea = document.getElementById('transaction-area');
@@ -12,13 +11,29 @@ loginBtn.addEventListener('click', function(){
 //deposit button event handler 
 const depositBtn = document.getElementById('deposit-btn');
 depositBtn.addEventListener('click', function(){
-    const amount = document.getElementById('deposit-amount').value;
-    const depoAmount = parseFloat(amount);
-    
-    const currentDepo = document.getElementById('current-depo').innerHTML;
-    const num = parseFloat(currentDepo);
-    const total = num + depoAmount;
-    console.log(total);
-    document.getElementById('current-depo').innerHTML = total;
-    document.getElementById('deposit-amount').value = '';
+    const depoAmount = getFromForm('deposit-amount');
+    updateSpanText('current-depo', depoAmount);
+    updateSpanText('current-balance', depoAmount);        
 });
+
+//Withdraw
+const withdrawBtn = document.getElementById('withdraw-btn');
+withdrawBtn.addEventListener('click', function(){
+    const withAmount = getFromForm('withdraw-amount');
+    updateSpanText('current-withdraw', withAmount);
+    updateSpanText('current-balance', -withAmount);    
+});
+
+function updateSpanText(spanID, addedNum){
+    const current = document.getElementById(spanID).innerHTML;
+    const balance = parseFloat(current);
+    const total = balance + addedNum;
+    document.getElementById(spanID).innerHTML = total;
+}
+
+function getFromForm(idName){
+    const amount = document.getElementById(idName).value;
+    const numAmount = parseFloat(amount);
+    document.getElementById(idName).value = '';
+    return numAmount;
+}
